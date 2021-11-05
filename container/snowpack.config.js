377efe6b8b8812230de83f4b166d1584d2588944
Aniket Pandharabale <aniketicloud@gmail.com>
2021-11-05 23:46:12 +0530

    container is running successfully as standalone app

---
+++ container/snowpack.config.js
@@ -0,0 +1,26 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mode: "development",
+  mount: {
+    // directory name: 'build directory'
+    public: '/',
+    // get everything form src to build
+    src: "/dist",
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+    port: 8080,
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
